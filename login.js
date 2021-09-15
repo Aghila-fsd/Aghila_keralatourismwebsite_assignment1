ff3241adceae91235b3794ff4506c441182280ab
Aghila <aghikrishna@gmail.com>
2021-09-15 23:49:35 +0530

    3rd committ

--- login.js
+++ login.js
@@ -1,53 +1,132 @@
-let email=document.getElementById("email");
+let email =document.getElementById("email");
+let username=document.getElementById("namee")
+let nameerror=document.getElementById("nameerror");
 let error=document.getElementById("error");
-let pwd=document.getElementById("password");
-let mobile=document.getElementById("mobile");
-//let regexp= /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
-//form validation
+let phno=document.getElementById("phno")
+let phoneerror=document.getElementById("phnoerror");
+let password=document.getElementById("psw")
+let confirm_password=document.getElementById("confpsw")
+let confirm_pass_error=document.getElementById("confpwderror")
+
+
+
+
+
+//pasword
+var Input = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+
+
+
+  function loadfunc()
+  {
+ username.innerText=null;
+   password.innerText="";
+  return true;
+  
+  }
+
 function validate()
 {
-   if(email.value.trim()==""||pwd.value.trim()==""||mobile.value.trim()=="")
+          let regexp = /^(?!\.)([[A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value.trim()))
     {
-        alert("feilds cann't be blank");
-       return false;
+     error.innerHTML=null;
+     
+      return true;
     }
     else
     {
-       return true;
-         //alert("validation is proper");
-     }
-}
-
-//end of form validation
-//email validation start
-
-
-    //let regexp=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z\-]{2,3}+)(.[a-z]{2,3}?)$/
-    let regexp=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}?\w+)*(.[a-z]{2,3}?)$/;
-
-    if(regexp.test(email.value))
-    {
-        error.innerHTML="valid";
-        error.style.color="green";
-        return true;
-    }
-    else if (regexp.test(email.value))
-    {
-        error.innerHTML="invalid";
-        error.style.color="red";
-        email.style.border='2px solid red';
-        return false;
-    }
-    else if(email.value=="")
-    {
-        alert("email can not be blank");
-        return false;
-    }
-    else
-    {
-        return true;
+      error.innerHTML="Email ID is Invalid";
+      error.style.color="red";
+      return false;
     }
 
 }
-//end of emailalidation
-//start password validation
+
+
+
+ 
+  
+  //When the user clicks on the password field, show the message box
+//   Input.onfocus = function() {
+//    document.getElementById("message").style.display = "block";
+//  }
+
+//  // When the user clicks outside of the password field, hide the message box
+  Input.onblur = function() {
+   document.getElementById("message").style.display = "none";
+ }
+
+// When the user starts to type something inside the password field
+Input.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(Input.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(Input.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(Input.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(Input.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+} 
+
+
+function matchPassword() {  
+  var pw1 = document.getElementById("psw");  
+  var pw2 = document.getElementById("confpsw");  
+  if(pw1.value == pw2.value)  
+  {   
+    confirm_pass_error.innerHTML=null;
+   // confirm_pass_error.style.color="green";
+    return true;
+  }
+   
+  else {  
+    confirm_pass_error.innerHTML="Password doesn't  match";
+    //alert("Password created successfully"); 
+    confirm_pass_error.style.color="red";
+ 
+    return false;
+  }  
+} 
+
+function main()
+{
+
+  return(validate());
+    
+  }
--- signup.js
+++ signup.js
@@ -1,135 +1,133 @@
-function formValidation()
-{
-var passid = document.registration.password;
-var cty=document.registration.city;
-var uadd1 = document.registration.addr1;
-var uadd2 = document.registration.addr2;
-var ustate = document.registration.state;
-var uzip = document.registration.zip;
-var uemail = document.registration.email;
-var uph=document.registration.mobile;
+// function formValidation()
+// {
+// var passid = document.registration.password;
+// var cty=document.registration.city;
+// var uadd1 = document.registration.addr1;
+// var uadd2 = document.registration.addr2;
+// var ustate = document.registration.state;
+// var uzip = document.registration.zip;
+// var uemail = document.registration.email;
+// var uph=document.registration.mobile;
 
-if(passid_validation(passid,8,12))
-{
-if(alphanumeric(uadd1))
-{ 
-if(alphanumeric(uadd2))
-{ 
-if(stateselect(ustate))
-{
-if(allnumeric(uzip))
-{
-if(ValidateEmail(uemail))
-{
-if(validph(uph))
-{
-}
-} 
-}
-}
-}
-}
-return false;
 
+let email =document.getElementById("email");
+let username=document.getElementById("namee")
+let nameerror=document.getElementById("nameerror");
+let error=document.getElementById("error");
+let phno=document.getElementById("phno")
+let phoneerror=document.getElementById("phnoerror");
+
+let confirm_password=document.getElementById("confpsw")
+let confirm_pass_error=document.getElementById("confpwderror")
+
+
+
+
+
+//pasword
+
+var myInput = document.getElementsByClassName("input_p");
+
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+
+function loadfunc()
+{  username.innerText=null;
+  password.innerText="";
+return true;
+ 
+}
+
+
+
+
+  
+
+function nameval()
+{
+  if(username.value.trim()=="")
+   {
+    nameerror.innerHTML="Please enter your name";
+    nameerror.style.color="red";
+      return false;
+   }
+   else
+   {
+    nameerror.innerHTML=null;
+     return true;
+   }
+}
+function validate()
+{
+          let regexp = /^(?!\.)([[A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value.trim()))
+    {
+     error.innerHTML=null;
+     
+      return true;
+    }
+    else
+    {
+      error.innerHTML="Email ID is Invalid";
+      error.style.color="red";
+      return false;
+    }
+
+}
+
+
+function phonenumb()
+{
+  var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  
+  
+  if(phoneno.test(phno.value.trim()))
+        {
+          phoneerror.innerHTML=null;
+      return true;
+        }
+      else
+        {
+
+          phoneerror.innerHTML="Phone number must be in the form xxx xxx xxxx,xxx-xxx-xxxx or xxx.xxx.xxxx";
+          phoneerror.style.color="red";
+          return false;
+   
+
+        }
+      }
+ 
+  
+ 
+
+function matchPassword() {  
+  var pw1 = document.getElementById("psw");  
+  var pw2 = document.getElementById("confpsw");  
+  if(pw1.value == pw2.value)  
+  {   
+    confirm_pass_error.innerHTML=null;
+   // confirm_pass_error.style.color="green";
+    return true;
+  }
+   
+  else {  
+    confirm_pass_error.innerHTML="Password doesn't  match";
+    //alert("Password created successfully"); 
+    confirm_pass_error.style.color="red";
+ 
+    return false;
+  }  
 } 
-function passid_validation(passid,mx,my)
-{
-var passid_len = passid.value.length;
-if (passid_len == 0 ||passid_len >= my || passid_len < mx)
-{
-alert("Password should not be empty / length be between "+mx+" to "+my);
-passid.focus();
-return false;
-}
-return true;
-}
-function alphanumeric(uadd1)
-{ 
-var letters = /^[0-9a-zA-Z]+$/;
-if(uadd1.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('User address must have alphanumeric characters only');
-uadd1.focus();
-return false;
-}
-}
-function alphanumeric(uadd2)
-{ 
-var letters = /^[0-9a-zA-Z]+$/;
-if(uadd2.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('User address must have alphanumeric characters only');
-uadd2.focus();
-return false;
-}
-}
-function stateselect(ustate)
-{
-if(ustate.value == "Default")
-{
-alert('Select your country from the list');
-ustate.focus();
-return false;
-}
-else
-{
-return true;
-}
-}
-function allnumeric(uzip)
-{ 
-var numbers = /^[0-9]+$/;
-if(uzip.value.match(numbers))
-{
-return true;
-}
-else
-{
-alert('ZIP code must have numeric characters only');
-uzip.focus();
-return false;
-}
-}
-function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
-function validph(uph)
-{
-    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-              if(uph.value.match(phoneno))
-              {
-               //alert("you enterd a valid phone number:"+form.mobile.value);
-                return true;
-              } 
-            else  
-            {   
-            alert("You have entered an invalid phone number!");
-            uph.focus();
-            return false;
-}
-else
-{
-alert('Form Succesfully Submitted');
-window.location.reload()
-return true;
-}
-}
+
+
+
+function validatebutton(){
+  var a= phonenumb();
+  var b = validate();
+  var c=nameval();
+  var d=matchPassword();
+   return a && b && c && d ;
 }
